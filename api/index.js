64e0c300be4e561eb4ed095ddb786a14b04328bd
dankukennedy <dankukennedy@email.com>
2024-03-27 14:39:28 +0000

    env

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
+// Importation of express
 import express from 'express';
+//Importation of Mongoose from mongoose
 import mongoose from 'mongoose';
-
-mongoose.connect('mongodb+srv://admin:admin@ims.inbmosu.mongodb.net/ims?retryWrites=true&w=majority').then( ()=>{
+// dotenv package and Webpack configuration, you can efficiently manage environment variables in your React.js
+import dotenv from 'dotenv';
+// dotenv package and Webpack configuration
+dotenv.config();
+//mongoose connection 
+mongoose.connect(process.env.MONGO).then( ()=>{
    console.log('MongooseDB is  connected');
 }).catch((err)=>
 {console.log(err)
 });
- 
+ //Dalaration of app on express
 const app = express();
-
+//app runging on port
 app.listen(3000, ()=> {
     console.log('server is loading on port 3000');
 });
