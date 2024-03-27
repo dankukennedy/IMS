e73fddf7170509665f64fcdae2ba7577485d50fd
dankukennedy <dankukennedy@email.com>
2024-03-27 14:22:34 +0000

    update on the server side

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb+srv://admin:admin@ims.inbmosu.mongodb.net/ims?retryWrites=true&w=majority').then( ()=>{
+   console.log('MongooseDB is  connected');
+}).catch((err)=>
+{console.log(err)
+});
+ 
 const app = express();
 
 app.listen(3000, ()=> {
