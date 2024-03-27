c1070d8eda9209a7b2ea051d5133302f48477d13
dankukennedy <dankukennedy@email.com>
2024-03-27 13:37:57 +0000

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import {BroserRouter, Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
-import SignUp from './pages/Signup'
+import SignUp from './pages/SignUp'
 import Project from './pages/Project'
 import Dashboard from './pages/Dashboard'
 
